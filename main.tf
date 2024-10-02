e3dbe1fd4f8f7101208c4173c0dabc09f3c4ec37
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-10-02 15:17:52 +0800

    Chore: Permission Updated

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
+# Main File Creation
 resource "local_file" "test_file" {
   filename = "test.txt"
   content = "This is a test file"
   file_permission = "0777"
 }
 
+# Updated file permissions
 resource "local_file" "test_file2" {
   filename = "test2.txt"
   content = "This is a test 2 file"
-  file_permission = "0777"
+  file_permission = "0700"
 }
